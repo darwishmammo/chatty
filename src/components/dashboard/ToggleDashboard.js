3f2aa82a28b0c021848a1db253ed2cc7892bf4ba
Darwish Mammo <darwish.mammo@gmail.com>
2021-02-26 13:44:57 +0100

    added sign out button and functionality to dashboard

--- src/components/dashboard/ToggleDashboard.js
+++ src/components/dashboard/ToggleDashboard.js
@@ -1,12 +1,18 @@
-import React from "react";
-import { Button, Icon } from "rsuite";
+import React, { useCallback } from "react";
+import { Alert, Button, Icon } from "rsuite";
 import { useMediaQuery, useModalState } from "../customHooks";
 import { Drawer } from "rsuite";
 import Dashboard from ".";
+import { auth } from "../../firebase";
 
 const ToggleDashboard = () => {
   const { isOpen, close, open } = useModalState();
   const isMobile = useMediaQuery("(max-width: 992px)");
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+    Alert.info("Signed out successfully!", 4000);
+    close();
+  }, [close]);
 
   return (
     <>
@@ -14,7 +20,7 @@ const ToggleDashboard = () => {
         <Icon icon="dashboard" /> Dashboard
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
