d905fea7e743171dbb786032227d684af9615c3c
Darwish Mammo <darwish.mammo@gmail.com>
2021-02-27 06:45:27 +0100

    added functionality to update user nickname in the datatabase

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { Button, Drawer, Divider } from "rsuite";
+import { Button, Drawer, Divider, Alert } from "rsuite";
 import { useProfile } from "../../context/profile.context";
+import { database } from "../../firebase";
 import StatefulInput from "../StatefulInput";
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
   const onSave = async (newData) => {
-    console.log(newData);
+    const userNicknameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child("name");
+
+    try {
+      await userNicknameRef.set(newData);
+
+      Alert.success("Nickname has been updated", 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
   };
 
   return (
