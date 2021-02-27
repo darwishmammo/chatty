804da508821eccdbf2349f2d314c1564916bdf8a
Darwish Mammo <darwish.mammo@gmail.com>
2021-02-27 06:07:39 +0100

    created a stateful input for editing profile

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,16 +1,29 @@
 import React from "react";
-import { Button, Drawer } from "rsuite";
+import { Button, Drawer, Divider } from "rsuite";
 import { useProfile } from "../../context/profile.context";
+import StatefulInput from "../StatefulInput";
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
+
+  const onSave = async (newData) => {
+    console.log(newData);
+  };
+
   return (
     <>
       <Drawer.Header>
         <Drawer.Title>Dashboard</Drawer.Title>
       </Drawer.Header>
       <Drawer.Body style={{ height: "80%" }}>
-        <h3>hello {profile.name}</h3>
+        <h3>Hello {profile.name}</h3>
+        <Divider />
+        <StatefulInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
