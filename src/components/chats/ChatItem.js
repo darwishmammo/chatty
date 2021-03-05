3552e43187400993bca7c3fc409f147c1213e8bb
Darwish Mammo <darwish.mammo@gmail.com>
2021-03-05 09:16:10 +0100

    displayed last meassage in chat item

--- src/components/chats/ChatItem.js
+++ src/components/chats/ChatItem.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useProfile } from "../../context/profile.context";
 import TimeAgo from "timeago-react";
+import ProfileAvatar from "../ProfileAvatar";
 
 const ChatItem = ({ chat }) => {
+  const { createdAt, lastMessage } = chat;
   const { profile } = useProfile();
   const recipient = chat.members.filter((m) => m !== profile.email);
   return (
@@ -10,13 +12,32 @@ const ChatItem = ({ chat }) => {
       <div className="d-flex justify-content-between align-items-center">
         <h3 className="text-disappear">{recipient[0]}</h3>
         <TimeAgo
-          datetime={new Date(chat.createdAt)}
+          datetime={
+            lastMessage ? new Date(lastMessage.createdAt) : new Date(createdAt)
+          }
           className="font-normal text-black-45"
         />
       </div>
 
       <div className="d-flex align-items-center text-black-70">
-        <span>No messages yet...</span>
+        {lastMessage ? (
+          <>
+            <div className="d-flex align-items-center">
+              <ProfileAvatar
+                src={lastMessage.author.avatar}
+                name={lastMessage.author.name}
+                size="sm"
+              />
+            </div>
+
+            <div className="text-disappear ml-2">
+              <div className="italic">{lastMessage.author.name}</div>
+              <span>{lastMessage.text}</span>
+            </div>
+          </>
+        ) : (
+          <span>No messages yet...</span>
+        )}
       </div>
     </div>
   );
